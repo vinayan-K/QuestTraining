1ed3da4065b69fff14cfc2491b96346d6c0e4986
vinayan-K <vinayank008@gmail.com>
2024-10-07 21:55:13 +0530

    Update in calculator

--- Day 4/Problems/Calculator.cs
+++ Day 4/Problems/Calculator.cs
@@ -56,6 +56,13 @@ namespace TwoDimensionalArray
 
      // Using singleline functions //
 
+            // static int add(int num1, int num2) => num1 + num2;               
+            // static int subtract(int num1, int num2) => num1 - num2;
+            // static int multiply(int num1, int num2) => num1 * num2;        
+            // static int division(int num1, int num2) => num1 / num2;
+
+            // static void Main(string[] args)
+            // {
             // Console.WriteLine("Enter two numbers : ");
             // int num1 = int.Parse(Console.ReadLine());
             // int num2 = int.Parse(Console.ReadLine());
